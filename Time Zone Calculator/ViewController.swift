9c76b019a351d00919e62f4d5afc202998286d6c
Akshay Kalia <akshaykalia.chd@gmail.com>
2021-02-18 22:09:58 +0530

    Initial Commit

---
+++ Time Zone Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Time Zone Calculator
+//
+//  Created by Akshay Kalia on 18/02/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Time Zone CalculatorTests/Time_Zone_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  Time_Zone_CalculatorTests.swift
+//  Time Zone CalculatorTests
+//
+//  Created by Akshay Kalia on 18/02/21.
+//
+
+import XCTest
+@testable import Time_Zone_Calculator
+
+class Time_Zone_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
